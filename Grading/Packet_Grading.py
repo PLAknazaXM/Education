0a1606b6a980028d8175330baf5a69fbd029534e
Kyle N <123481268+PLAknazaXM@users.noreply.github.com>
2023-06-19 13:05:33 -0400

    Add files via upload

---
+++ Grading/Packet_Grading.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import numpy as np
+
+st.set_page_config(page_title="Packet Grading App",
+                   page_icon=":check_mark:", layout="wide")
+
+st.title('Packet Grading Program')
+#st.sidebar.success("Select a page above")
+
+
+st.markdown('''This provides a quick and simple method to grade
+packets, providing fractional and percentage grades.''')
+
+Total = st.number_input('Enter the total point value of the packet:')
+
+Points_float = (0,0)
+
+Earn = st.text_input('''Enter the number of points earned on each page, separated by commas, spaces, or dashes.
+Ex: 1,5,13 OR 1 5 13 OR 1-5-13''')
+
+try:
+    Points = Earn.split('-')
+    Points_float = [float(x) for x in Points]
+except:
+    try:
+        Points = Earn.split(' ')
+        Points_float = [float(x) for x in Points]
+    except:
+        try:
+            Points = Earn.split(',')
+            Points_float = [float(x) for x in Points]
+        except:
+            st.title('Invalid method of entering points earned!')
+
+Total_Earn = sum(Points_float)
+
+if Total == 0:
+    st.write('Configure total points of assignment')
+else:
+    st.write('''Fractional Grade:''', Total_Earn, '/', Total)
+    st.write('Percentage Grade: ', round(Total_Earn/Total*100,2), '%')
